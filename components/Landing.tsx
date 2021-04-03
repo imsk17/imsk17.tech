0ba49483ce24d29b6bf086d5ef3f3e35af7eda54
imsk17 <imsumitkumar17@gmail.com>
2021-04-03 22:44:14 +0530

    :wrench: Some Touch Up

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,31 +1,34 @@
-import style from "./Landing.module.css"
-import Image from "next/image"
+import style from "./Landing.module.css";
+import Image from "next/image";
 
 export default function Landing() {
-    return (
-        <div className={style.container}>
-            <div className={style.gopher}>
-            <Image alt="gopher"  src="/Gopher.svg" width="316" height="428"></Image>
-            </div>
-            <p className={style.loves}>
-                ANDROID AND BACKEND ENGINEER
-            </p>
-            <div className={style.titlecon}>
-            <p className={style.title}>
-               Sumit
-               <p>     Kumar</p>
-            </p>
-            </div>
-            <p className={style.nav}>
-            <a href="#About">About</a> /&nbsp;
-                <a href="#Contact">Contact</a> /&nbsp;
-                <a href="#Work">Work</a> /&nbsp;
-                <a href="/Resume.pdf">Resume</a> /&nbsp;
-                <a href="#Tools">Tools</a> 
-            </p>
-            <div className={style.android}>
-            <Image  alt="android" src="/Android.svg" width="250" height="150"></Image>
-            </div>
-        </div>
-    )
+  return (
+    <div className={style.container}>
+      <div className={style.gopher}>
+        <Image alt="gopher" src="/Gopher.svg" width="316" height="428"></Image>
+      </div>
+      <strong className={style.loves}>ANDROID AND BACKEND ENGINEER</strong>
+      <div className={style.titlecon}>
+        <p className={style.title}>
+          Sumit
+          <p> Kumar</p>
+        </p>
+      </div>
+      <p className={style.nav}>
+        <a href="#About">About</a> /&nbsp;
+        <a href="#Contact">Contact</a> /&nbsp;
+        <a href="#Work">Work</a> /&nbsp;
+        <a href="/Resume.pdf">Resume</a> /&nbsp;
+        <a href="#Tools">Tools</a>
+      </p>
+      <div className={style.android}>
+        <Image
+          alt="android"
+          src="/Android.svg"
+          width="250"
+          height="150"
+        ></Image>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <div>
       <Head>
         <link rel="shortcut icon" href="/favicon.svg" />
-        <title>Sumit Kumar</title>
+        <title>Sumit Kumar (imsk17)</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Sumit's personal portfolio" />
+        <meta name="description" content="Home | Sumit's Personal Portfolio" />
       </Head>
       <Ham />
       <Landing />
